01e6e5d6e984b5f5314e1cb3bffa948b10d6e796
Jaewoong Kim <rorhcdream@gmail.com>
2021-08-01 11:41:59 +0900

    Fix SearchControllerTest to test only the controller

--- src/test/java/gg/cse/web/SearchControllerTest.java
+++ src/test/java/gg/cse/web/SearchControllerTest.java
@@ -1,37 +1,44 @@
 package gg.cse.web;
 
 import gg.cse.dto.riotDto.MatchDto;
+import gg.cse.service.search.SearchService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.LinkedList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.any;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@WebMvcTest(SearchController.class)
 public class SearchControllerTest {
-    @LocalServerPort
-    private int port;
+    @MockBean
+    SearchService searchService;
 
     @Autowired
-    private TestRestTemplate restTemplate;
+    private MockMvc mockMvc;
 
     @Test
     public void search_test() throws Exception {
-        String summonerName = "Hide on bush";
-        String url = "http://localhost:" + port + "/api/v1/search/" + summonerName;
+        List<MatchDto> matchDtos = new LinkedList<>();
+        for(int i = 0; i < 20; i++) {
+            matchDtos.add(new MatchDto());
+        }
+        when(searchService.search(any(String.class))).thenReturn(matchDtos);
 
-        List<MatchDto> matchDtos =
-                restTemplate.exchange(url, HttpMethod.GET, null,
-                    new ParameterizedTypeReference<List<MatchDto>>() {}
-                ).getBody();
-        assertNotNull(matchDtos);
-        assertEquals(matchDtos.size(), 20);
+        String summonerName = "Hide on bush";
+
+        mockMvc.perform(get("/api/v1/search/" + summonerName))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[19].info").isEmpty());
     }
 }
